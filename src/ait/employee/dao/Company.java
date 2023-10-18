0bae0975fcf46de9358aac7dcc9b2426d8eebefd
nikelev <nikelev777@gmail.com>
2023-10-18 18:19:27 +0200

    commit1

---
+++ src/ait/employee/dao/Company.java
@@ -0,0 +1,27 @@
+package ait.employee.dao;
+
+import ait.employee.model.Employee;
+
+public interface Company {
+    boolean addEmployee(Employee employee);
+
+    Employee removeEmployee(int id);
+
+    Employee findEmployee(int id);
+
+    double totalSalary();
+
+    int quantity();
+
+    double avgSalary();
+
+    double totalSales();
+
+    void printEmployees();
+
+    Employee[] findEmployeesHoursGreaterThan(int hours);
+
+    Employee[] findEmployeesSalaryRange(int minSalary, int maxSalary);
+
+
+}
